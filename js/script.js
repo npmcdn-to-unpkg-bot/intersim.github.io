f6097e737057d539f92d0c11bb40b55aa69e040b
intersim <emily.intersimone@gmail.com>
2016-06-26 16:04:09 -0400

    fixed z index issue

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(window).scroll(function() {
-    if ($(window).scrollTop() > 100) {
-        $('#fixed-nav').fadeIn('fast', function () {
+    if ($(window).scrollTop() > 170) {
+        $('#fixed-nav').fadeIn(200, function () {
           $(this).removeClass('hide-nav');
         })
     } else {
-        $('#fixed-nav').fadeOut('fast', function () {
+        $('#fixed-nav').fadeOut(200, function () {
           $(this).addClass('hide-nav');
         })
     }
