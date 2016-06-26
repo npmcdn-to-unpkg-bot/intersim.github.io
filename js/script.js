d92c152ba578acf61728d22290660565f583cde9
intersim <emily.intersimone@gmail.com>
2016-06-26 19:34:50 -0400

    fix weird nav firefox bug

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(window).scroll(function() {
     if ($(window).scrollTop() > 100) {
         $('#fixed-nav').fadeIn(200, function () {
-          $(this).removeClass('hide-nav');
+          $(this).removeClass('hide-nav').css('display', 'flex');
         })
     } else {
         $('#fixed-nav').fadeOut(200, function () {
