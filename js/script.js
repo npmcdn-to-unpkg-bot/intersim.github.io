0af71cccc46a033359b5aa0bc4b355e19884e0cc
intersim <emily.intersimone@gmail.com>
2016-06-26 16:21:04 -0400

    fixed in-page link issue

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-    if ($(window).scrollTop() > 170) {
+    if ($(window).scrollTop() > 100) {
         $('#fixed-nav').fadeIn(200, function () {
           $(this).removeClass('hide-nav');
         })
